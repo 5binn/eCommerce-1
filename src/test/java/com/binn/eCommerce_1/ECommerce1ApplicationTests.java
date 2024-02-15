4925394635292b9d984ad69d94677b19f6ceacdd
본ì5binn <dhxnejqls13@naver.com>
2024-02-15 17:42:48 +0900

    base_entity#1

---
+++ src/test/java/com/binn/eCommerce_1/ECommerce1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binn.eCommerce_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerce1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
