f1241c2682d6d1683575f3e10d64079862de3aa9
5binn <dhxnejqls13@naver.com>
2024-02-16 15:43:49 +0900

    feat/3#1

--- src/main/java/com/binn/eCommerce_1/domain/product/controller/ProductController.java
+++ src/main/java/com/binn/eCommerce_1/domain/product/controller/ProductController.java
@@ -1,11 +1,26 @@
 package com.binn.eCommerce_1.domain.product.controller;
 
+import com.binn.eCommerce_1.domain.product.entity.Product;
 import com.binn.eCommerce_1.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/product")
 public class ProductController {
     private final ProductService productService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Product> productList = this.productService.list();
+        model.addAttribute("productList",productList);
+        return "product_list";
+    }
+
 }
--- src/main/java/com/binn/eCommerce_1/domain/product/service/ProductService.java
+++ src/main/java/com/binn/eCommerce_1/domain/product/service/ProductService.java
@@ -1,12 +1,19 @@
 package com.binn.eCommerce_1.domain.product.service;
 
+import com.binn.eCommerce_1.domain.product.entity.Product;
 import com.binn.eCommerce_1.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public List<Product> list() {
+        return this.productRepository.findAll();
+    }
 }
